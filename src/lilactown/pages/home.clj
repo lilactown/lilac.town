efd8a9c557ca603ea948ad36441ba573c0c316cb
Will Acton <will@willacton.com>
2018-02-16 14:19:34 -0800

    Cool articles

--- src/lilactown/pages/home.clj
+++ src/lilactown/pages/home.clj
@@ -12,40 +12,65 @@
 
 (defn styles [bg-color]
   [[:* {:box-sizing "border-box"}]
-             [:body {:font-family "Roboto Condensed, sans-serif"
-                     :background-color bg-color
-                     :color "#3b3b3b"}]
-             [:h1 :h2 :h3 :h4 {:font-family "Roboto Slab, serif"}]
-             [:a {:color "#371940"
-                  :text-decoration "none"}
-              [:&:hover {:color "#9a549a"}]]
-             [:#main {:max-width "650px"
-                      :margin "40px auto"}]
-             ;; [:.title {:text-align "center"}]
-             ;; [:.tag-line {:text-align "center"}]
-             [:.repos {:display "grid"
-                       :grid-template-columns "1fr 1fr 1fr"
-                       :grid-column-gap "15px"
-                       :grid-row-gap "15px"}
-              ]
-             [:.repo {:border "1px solid #555"
-                      :border-radius "4px"
-                      :padding "10px 12px 10px"
-                      :background-color "rgba(100, 100, 100, .1)"
-                      :box-shadow "1px 1px 3px rgba(0, 0, 0, .5)"
-                      :display "grid"
-                      :grid-row-gap "8px"
-                      :grid-auto-rows "minmax(20px, auto)"}
-              [:.date {:position "relative"}
-               [:.display {:position "absolute"
-                           :bottom 0}]]
-              ;; [:.title {:padding "0 0 5px 0"}]
-              ;; [:.desc {:padding "0 0 8px 0"}]
-              [:.stars {:float "right"}]
-              [:.star-glyph {:display "inline-block"
-                             :vertical-align "text-bottom"
-                             :fill "currentColor"
-                             :margin-right "5px"}]]])
+   [:body {:font-family "Roboto Condensed, sans-serif"
+           :background-color bg-color
+           :color "#3b3b3b"}]
+   [:h1 :h2 :h3 :h4 {:font-family "Roboto Slab, serif"}]
+   [:a {:color "#371940"
+        :text-decoration "none"}
+    [:&:hover {:color "#9a549a"}]]
+   [:#main {:max-width "650px"
+            :margin "40px auto"}]
+   ;; [:.title {:text-align "center"}]
+   ;; [:.tag-line {:text-align "center"}]
+   [:.repos {:display "grid"
+             :grid-template-columns "1fr 1fr 1fr"
+             :grid-column-gap "15px"
+             :grid-row-gap "15px"}
+    ]
+   [:.repo {:border "1px solid #555"
+            :border-radius "4px"
+            :padding "10px 12px 10px"
+            :background-color "rgba(100, 100, 100, .1)"
+            :box-shadow "1px 1px 3px rgba(0, 0, 0, .5)"
+            :display "grid"
+            :grid-row-gap "8px"
+            :grid-auto-rows "minmax(20px, auto)"}
+    [:.date {:position "relative"}
+     [:.display {:position "absolute"
+                 :bottom 0}]]
+    ;; [:.title {:padding "0 0 5px 0"}]
+    ;; [:.desc {:padding "0 0 8px 0"}]
+    [:.stars {:float "right"}]
+    [:.star-glyph {:display "inline-block"
+                   :vertical-align "text-bottom"
+                   :fill "currentColor"
+                   :margin-right "5px"}]]
+   [:.articles {:list-style "none"
+                :padding "0"
+                :margin "0"}]
+   [:.article {:padding "5px 0"}
+    [:.title {:font-size "1.5rem"
+              :display "inline-block"}]
+    [:.date {:margin-right "12px"
+             :font-size "1.2rem"
+             :vertical-align "unset"
+             :display "inline-block"
+             :width "3rem"
+             :border-right "1px solid #3b3b3b"
+             ;; :text-align "center"
+             }]
+    [:.categories {:list-style "none"
+                   :padding "0"
+                   :margin-top "3px"}
+     [:.category {:display "inline-block"
+                  :text-align "center"
+                  :padding "3px 8px"
+                  :margin "0 3px 0 0"
+                  :background-color "rgba(0, 0, 0, .5)"
+                  :border-radius "4px"
+                  :color "#DCD0FF"
+                  :font-size ".8rem"}]]]])
 
 (def query "Pinned repos"
   (s/replace
@@ -120,16 +145,18 @@
    (:entries (feed/parse-feed (feed/uri-stream "https://medium.com/feed/@lilactown")))))
 
 (defn article-category [{:keys [name]}]
-  [:span name])
+  [:li.category name])
 
 (defn format-article-date [d]
   (f/unparse (f/formatter "MMM YYYY") (clj-time.coerce/from-date d)))
 
 (defn article [{:keys [title link published-date categories]}]
-  [:div.article
-   [:div.title [:h3 [:a {:href link :target "_blank"} title]]]
-   [:div.categories (map article-category categories)]
-   [:div.date (format-article-date published-date)]])
+  [:li.article
+   [:div.date (format-article-date published-date)]
+   [:div.title
+    [:div
+     [:span [:a {:href link :target "_blank"} title]]]
+    [:ul.categories (map article-category categories)]]])
 
 (defn fetch []
   (let [data-ch (async/chan)]
@@ -165,7 +192,7 @@
       [:link {:href "https://fonts.googleapis.com/css?family=Roboto+Condensed|Roboto+Slab"
               :rel "stylesheet"}]
       [:style
-       (garden/css (styles (or bgcolor "#DCD0FF") ;; "#C8A2C8"
+       (garden/css (styles "#DCD0FF" ;; "#C8A2C8"
                     ))]]
      [:body
       [:div#main
@@ -178,7 +205,6 @@
          (map repo repos)]]
        [:div
         [:h2 "Articles"]
-        [:div.articles
+        [:ul.articles
          (map article articles)]]
-       [:div
-        [:h2 "Cool stuff"]]]]]))
+       ]]]))
