fb0822e71ad1faf60207214a201867c61e308060
Will Acton <will@willacton.com>
2018-02-21 09:36:02 -0800

    describe frames as deltas

--- src/lilactown/client/title/core.cljs
+++ src/lilactown/client/title/core.cljs
@@ -10,10 +10,25 @@
                     (set! (.-style canvas-) "vertical-align: text-bottom")
                     canvas-))
 
-(defonce context (.getContext canvas "2d"))
+(defonce context (.getContext canvas "2d" (clj->js {:alpha false})))
 
 (defn clear []
   (.clearRect context 0 0 (.-width canvas) (.-height canvas)))
+
+(defn rect! [ctx {:keys [x y width height fill stroke]}]
+  (when fill
+    (let [old-style (.-fillStyle ctx)]
+      (set! (.-fillStyle ctx) fill)
+      (.fillRect ctx x y width height)
+      (set! (.-fillStyle ctx) old-style)
+      ctx))
+  (when stroke
+    (let [old-style (.-strokeStyle ctx)]
+      (js/console.log old-style stroke)
+      (set! (.-strokeStyle ctx) stroke)
+      (.strokeRect ctx x y width height)
+      (set! (.-strokeStyle ctx) old-style)
+      ctx)))
 
 (defn text! [ctx {:keys [text x y max-width font fill]}]
   (let [old-font (.-font ctx)
@@ -30,26 +45,44 @@
 
 (def bottom (- (.-height canvas) 9))
 
-(def frames [[(merge {:text "lilac.town"
-                      :x 0
-                      :y bottom} text-style ;; {:fill "red"}
-                     )]
-             (map #(merge % text-style {:y bottom})
-                  [{ :text "l" :x 0}
-                   { :text "i" :x 10}
-                   { :text "l" :x 20.5}
-                   { :text "a" :x 30.8}
-                   { :text "c" :x 48.3}
-                   { :text "." :x 65.5}
-                   { :text "t" :x 73.2}
-                   { :text "o" :x 84.7}
-                   { :text "w" :x 102.5}
-                   { :text "n" :x 129}])])
+(def initial-text (merge {:text "lilac.town"
+                          :x 0
+                          :y bottom} text-style ;; {:fill "red"}
+                         ))
 
-(defn draw []
+(def initial-letters (map #(merge % text-style {:y bottom})
+                          [{:text "l" :x 0}
+                           {:text "i" :x 10}
+                           {:text "l" :x 20.5}
+                           {:text "a" :x 30.8}
+                           {:text "c" :x 48.3}
+                           {:text "." :x 65.5}
+                           {:text "t" :x 73.2}
+                           {:text "o" :x 84.7}
+                           {:text "w" :x 102.5}
+                           {:text "n" :x 129}]))
+
+(def _ identity)
+
+(def frames [[{:y 10} _ _ _ _ _ _ _ _ _]])
+
+(defn apply-frame [initial frame]
+  (->> (map vector frame initial)
+        (map (fn [[f s]]
+              (if (map? f)
+                (merge-with + f s)
+                (f s))))
+        ))
+
+(defn draw-frame! [frame]
+  (dorun (map #(text! context %) frame)))
+
+(defn draw! []
   (js/console.log "drawing")
-  ;(doall (map #(text! context %) (first frames)))
-  (doall (map #(text! context %) (second frames)))
+  (rect! context {:x 0 :y 0 :width (.-width canvas) :height (.-height canvas)
+                  :fill "#DCD0FF"})
+  (draw-frame! initial-letters)
+  (draw-frame! (apply-frame initial-letters (first frames)))
   )
 
 (defn ^:export init []
@@ -58,11 +91,11 @@
   (let [title-container (.querySelector js/document ".title")]
     (set! (.-innerHTML title-container) "")
     (.appendChild title-container canvas))
-  (draw))
+  (draw!))
 
 (defn start []
   (js/console.log "starting")
-  (draw)
+  (draw!)
   )
 
 (defn stop []
