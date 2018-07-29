e0848048b9f2b665aee91fdce564792af7e37028
Will Acton <will@willacton.com>
2018-07-29 09:49:54 -0700

    organization and magic

--- src/lilactown/dom.clj
+++ src/lilactown/dom.clj
@@ -4,3 +4,20 @@
   `(fn [args#]
      (let [~@props (~'js->clj args# :keywordize-keys true)]
        ~@body)))
+
+(defmacro send-this [args f]
+  (let [this (gensym 'this)]
+    `(fn ~args
+       (~'this-as ~this
+        ~(when (count args)
+           `(apply ~f ~this ~@args)
+           `(~f ~this))))))
+
+(defmacro this [& syms]
+  `(get-in$ ~'this ~@(map name syms)))
+
+(defmacro props [& keys]
+  `(props* ~'this ~@keys))
+
+(defmacro children []
+  `(children* ~'this))
--- src/lilactown/dom.cljs
+++ src/lilactown/dom.cljs
@@ -1,11 +1,8 @@
 (ns lilactown.dom
   (:require [react :as react]
+            [goog.object :as gobj]
             [create-react-class :as create-react-class])
   (:require-macros [lilactown.dom]))
-
-(defn component
-  [schema]
-  (create-react-class (clj->js schema)))
 
 (defn factory [component]
   (let [f (react/createFactory component)]
@@ -15,6 +12,43 @@
 
         ;; props are children
         (apply f nil props children)))))
+
+(defn- assoc-when
+  "Assocs m with k v, only when pred is true"
+  [m pred k v]
+  (if pred
+    (assoc m k v)
+    m))
+
+(defn- bind-method
+  "Creates "
+  [m k]
+  (assoc-when
+   m
+   (m k)
+   k (lilactown.dom/send-this [] (m k))))
+
+(def get-in$ gobj/getValueByKeys)
+
+(defn props* [this & keys]
+  (apply get-in$ this "props" keys))
+
+(defn children* [this]
+  (props* this "children"))
+
+(defn state* [this & keys]
+  (apply get-in$ "state" keys))
+
+(defn component
+  [schema]
+  (factory
+   (create-react-class
+    (-> schema
+        (bind-method :getInitialState)
+        (bind-method :componentDidMount)
+        (bind-method :componentWillUnmount)
+        (bind-method :render)
+        (clj->js)))))
 
 (def div (factory "div"))
 
