edf80168a0621e220c01aef066841c87195395d4
Will Acton <will@willacton.com>
2018-08-08 11:56:01 -0700

    all the dom

--- src/lilactown/react/dom.cljs
+++ src/lilactown/react/dom.cljs
@@ -1,11 +1,14 @@
 (ns lilactown.react.dom
-  (:require [lilactown.react :as react]))
+  (:refer-clojure :exclude [map meta time])
+  (:require [lilactown.react :as react])
+  (:require-macros [lilactown.react.dom]))
 
+;; (def div (react/factory "div"))
 
-(def div (react/factory "div"))
+;; (def h1 (react/factory "h1"))
 
-(def h1 (react/factory "h1"))
+;; (def span (react/factory "span"))
 
-(def span (react/factory "span"))
+;; (def button (react/factory "button"))
 
-(def button (react/factory "button"))
+(lilactown.react.dom/make-factories)
