5b42bdfb4927dee6b53de77cb55e0be1612ea151
Will Acton <will@willacton.com>
2018-08-09 11:32:25 -0700

    revert back to using react pts component

--- src/lilactown/client/pts.cljs
+++ src/lilactown/client/pts.cljs
@@ -1,78 +1,48 @@
 (ns lilactown.client.pts
-  (:require ;; [lilactown.react :as r]
-            ;; [lilactown.react.dom :as dom]
-            ;; [react-dom :as react-dom]
-            ;; ["pts" :as pts]
-            ))
+  (:require [lilactown.react :as r]
+            [lilactown.react.dom :as dom]
+            [react-dom :as react-dom]
+            ["pts" :as pts]))
 
-;; (r/defcomponent Pts
-;;   :create-chart
-;;   (r/send-this
-;;    (fn [this]
-;;      ;; setup
-;;      (let [space (-> (r/this :pts-canvas)
-;;                      (pts/CanvasSpace.)
-;;                      (.setup #js {:bgColor "#fff"
-;;                                   :resize true
-;;                                   :retina true}))
-;;            form (.getForm space)]
-;;        (r/set-this! :space space)
-;;        (r/set-this! :form form)
+(r/defcomponent Pts
+  :create-chart
+  (r/send-this
+   (fn [this]
+     ;; setup
+     (let [space (-> (r/this :pts-canvas)
+                     (pts/CanvasSpace.)
+                     (.setup #js {:bgColor "#fff"
+                                  :resize true
+                                  :retina true}))
+           form (.getForm space)]
+       (r/set-this! :space space)
+       (r/set-this! :form form)
 
-;;        (.add ^js space
-;;              (fn []
-;;                (.point ^js form
-;;                        (.-pointer ^js space) 10)))
+       (.add ^js space
+             (fn []
+               (.point ^js form
+                       (.-pointer ^js space) 10)))
 
-;;        (-> space
-;;            (.bindMouse)
-;;            (.bindTouch))
+       (-> space
+           (.bindMouse)
+           (.bindTouch))
 
-;;        (-> space
-;;            (.playOnce 200)))))
+       (-> space
+           (.playOnce 200)))))
 
-;;   :componentDidMount
-;;   (fn [this]
-;;     ((r/this :create-chart)))
+  :componentDidMount
+  (fn [this]
+    ((r/this :create-chart)))
 
-;;   :componentDidUpdate
-;;   (fn [this]
-;;     (js/console.log "updated")
-;;     ((r/this :space :playOnce) 0))
+  :componentDidUpdate
+  (fn [this]
+    (js/console.log "updated")
+    ((r/this :space :playOnce) 0))
 
-;;   :render
-;;   (fn [this]
-;;     (dom/div
-;;      (dom/canvas {:ref #(r/set-this! :pts-canvas %)}))))
-
-(def pts (.-Pts js/window))
-
-;; set the namespace
-(.namespace pts js/window)
-
-(def CanvasSpace (.-CanvasSpace pts))
-
-(def canvas (let [-canvas (.createElement js/document "canvas")]
-              (set! (.-id -canvas) "pts-canvas")
-              -canvas))
-
-(defn render []
-  (let [space (-> (CanvasSpace. "#pts-canvas")
-                  (.setup #js {:retina true
-                               :resize true}))
-        form (.getForm ^js space)]
-    ;; animation
-    (.add space #(do
-                   (js/console.log "doin'")
-                   (.point form (.-pointer ^js space) 10)))
-
-    ;;start
-    (-> ^js space
-        (.playOnce 0)
-        (.bindMouse)
-        (.bindTouch))))
+  :render
+  (fn [this]
+    (dom/div
+     (dom/canvas {:ref #(r/set-this! :pts-canvas %)}))))
 
 (defn ^:export start! [node]
-  (set! (.-innerHTML node) "")
-  (.appendChild node canvas)
-  (render))
+  (react-dom/render (Pts) node))
