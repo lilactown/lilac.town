990d46b8564a69032d6aa7726fec492da5801b7e
Will Acton <will@willacton.com>
2018-08-10 08:04:51 -0700

    dom/<> for react/Fragment

--- src/lilactown/react/dom.cljs
+++ src/lilactown/react/dom.cljs
@@ -1,6 +1,9 @@
 (ns lilactown.react.dom
   (:refer-clojure :exclude [map meta time])
-  (:require [lilactown.react :as react])
+  (:require [lilactown.react :as r]
+            [react :as react])
   (:require-macros [lilactown.react.dom]))
 
 (lilactown.react.dom/make-factories)
+
+(def <> (r/factory react/Fragment))
