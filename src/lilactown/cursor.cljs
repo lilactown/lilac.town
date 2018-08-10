a1af3def60b814c3c1f9bccfe6114e3c59bcf9a2
Will Acton <will@willacton.com>
2018-08-10 08:15:54 -0700

    ReadCursor and lilactown.cursor/select

---
+++ src/lilactown/cursor.cljs
@@ -0,0 +1,39 @@
+(ns lilactown.cursor)
+
+(deftype ReadCursor [ref select meta]
+  Object
+  (equiv [this other]
+    (-equiv this other))
+
+  IEquiv
+  (-equiv [this other]
+    (identical? this other))
+
+  IMeta
+  (-meta [_] meta)
+
+  IHash
+  (-hash [this] (goog/getUid this))
+
+  IDeref
+  (-deref [_]
+    (select (-deref ref)))
+
+  IWatchable
+  (-add-watch [this key run]
+    (add-watch ref (list this key)
+               (fn [_ _ old-v new-v]
+                 (let [old (select old-v)
+                       new (select new-v)]
+                 (when (not= old new)
+                   (run key this old new))))))
+  (-remove-watch [this key]
+    (remove-watch ref (list this key))
+    this))
+
+(defn select
+  ([ref selector]
+   (ReadCursor. ref selector nil))
+
+  ([ref selector & {meta :meta}]
+   [ReadCursor. ref selector meta]))
--- src/lilactown/workspace/react.cljs
+++ src/lilactown/workspace/react.cljs
@@ -2,7 +2,8 @@
   (:require [nubank.workspaces.core :as ws]
             [nubank.workspaces.card-types.react :as ct.react]
             [lilactown.react :as r]
-            [lilactown.react.dom :as dom]))
+            [lilactown.react.dom :as dom]
+            [lilactown.cursor :as cursor]))
 
 (defn on-change! [ref ev]
   (let [v (.. ev -target -value)]
@@ -20,3 +21,36 @@
 (ws/defcard Input
   (ct.react/react-card
    (Input)))
+
+(defonce app-db (atom {:foo "bar"
+                       :baz 0}))
+
+(def foo-db (cursor/select app-db :foo))
+
+(def baz-db (cursor/select app-db :baz))
+
+(r/defreactive Foo
+  :watch (fn [_] {:state foo-db})
+  :render
+  (fn [this {state :state}]
+    (println "foo render")
+    (dom/div
+     (dom/div "Foo: " @state)
+     (dom/button {:onClick #(swap! app-db assoc :foo "42")}
+                 "Universe"))))
+
+(r/defreactive Baz
+  :watch (fn [_] {:state baz-db})
+  :render
+  (fn [this {state :state}]
+    (println "baz render")
+    (dom/div
+     (dom/div "Baz: " @state)
+     (dom/button {:onClick #(swap! app-db update :baz inc)}
+                 "+"))))
+
+(ws/defcard App-db
+  (ct.react/react-card
+   (dom/div
+    (Foo)
+    (Baz))))
