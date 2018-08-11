882ead5f5d664e9025378e69305c49473b71327d
Will Acton <will@willacton.com>
2018-08-10 20:10:25 -0700

    context state management

--- src/lilactown/workspace/react.cljs
+++ src/lilactown/workspace/react.cljs
@@ -1,6 +1,7 @@
 (ns lilactown.workspace.react
   (:require [nubank.workspaces.core :as ws]
             [nubank.workspaces.card-types.react :as ct.react]
+            [react :as react]
             [lilactown.react :as r]
             [lilactown.react.dom :as dom]
             [lilactown.cursor :as cursor]))
@@ -25,53 +26,76 @@
 
 ;; Example of a redux-like pattern
 
-(defn select-props [structure]
-  (fn [this]
-    (reduce-kv
-     (fn [m k f]
-       (assoc m
-              k
-              (cursor/select
-               (r/get-in$ this "props" (name k)) f)))
-     {}
-     structure)))
+(defn db-provider [Provider]
+  (r/reactive-component
+   {:watch (fn [this]
+             {:db (r/props :db)})
+    :render
+    (fn [this {db :db}]
+      (Provider {:value {:db @db
+                         :dispatch (r/props :dispatch)}}
+                (r/children)))}))
 
-(defmulti dispatch (fn [action ref payload]
+(defn db-context
+  ([]
+   (let [context (react/createContext)
+         Provider (db-provider (r/factory (.-Provider context)))
+         Consumer (r/factory (.-Consumer context))]
+     [Provider Consumer])))
+
+(defmulti dispatch (fn [ref action payload]
                      action))
 
-(defmethod dispatch :foo/update
-  [action ref _]
-  (swap! ref assoc :foo 42))
+(defmethod dispatch :name/update
+  [ref action payload]
+  (swap! ref assoc :name payload))
 
-(defmethod dispatch :baz/update
-  [action ref _]
-  (swap! ref update :baz inc))
+(defmethod dispatch :count/inc
+  [ref action _]
+  (swap! ref update :count inc))
 
-(r/defrc Foo
-  {:watch (select-props
-           {:db #(get-in % [:foo :bar])})}
-  [{dispatch :dispatch} {state :db}]
+(def my-context (db-context))
+
+(r/defnc Foo
+  [{:keys [name on-change]}]
+  (println "Foo")
   (dom/div
-   (dom/div "Foo: " (prn-str @state))
-   (dom/button {:onClick #(dispatch :foo/update)}
-               "Universe")))
+   (dom/div "Greetings, " name)
+   (dom/input {:value name
+               :onChange #(on-change (.. % -target -value))})))
 
-(r/defrc Baz
-  {:watch (select-props
-           {:db :baz})}
-  [{dispatch :dispatch} {state :db}]
+(r/defnc FooContainer []
+  (let [[_ DbConsumer] my-context]
+    (DbConsumer
+     (fn [{db :db
+           dispatch :dispatch}]
+       (Foo {:name (:name db)
+             :on-change ^:no-update #(dispatch :name/update %)})))))
+
+(r/defnc Bar
+  [{:keys [count on-click]}]
+  (println "Bar")
   (dom/div
-   (dom/div "Baz: " @state)
-   (dom/button {:onClick #(dispatch :baz/update)}
-               "+")))
+   (dom/div "Count: " count)
+   (dom/button {:onClick #(on-click)}
+               "inc")))
+
+(r/defnc BarContainer []
+  (let [[_ DbConsumer] my-context]
+    (DbConsumer
+     (fn [{db :db dispatch :dispatch}]
+       (Bar {:count (:count db)
+             :on-click ^:no-update #(dispatch :count/inc)})))))
 
 (ws/defcard App-db
   (ct.react/react-card
-   (let [app-db (atom {:foo {:bar "asdf"}
-                       :baz 0})
-         dispatcher #(dispatch %1 app-db %2)]
-     (dom/div
-      (Foo {:db app-db
-            :dispatch dispatcher})
-      (Baz {:db app-db
-            :dispatch dispatcher})))))
+   (let [[DbProvider _] my-context
+         db (atom {:name "wat"
+                   :count 0})
+         dispatch (partial dispatch db)]
+     (DbProvider
+      {:db db
+       :dispatch dispatch}
+      (dom/div
+       (FooContainer)
+       (BarContainer))))))
