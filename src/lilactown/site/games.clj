43fe63bac970812585b8d04d090a34bdf26c9316
Will Acton <will@willacton.com>
2018-08-14 13:47:34 -0700

    add sweeper module

--- src/lilactown/site/games.clj
+++ src/lilactown/site/games.clj
@@ -18,7 +18,10 @@
         :text-decoration "none"}
     [:&:hover {:color "#9a549a"}]]
    [:#main {:max-width "670px"
-            :margin "40px auto"}]])
+            :margin "40px auto"}]
+   [:#sweeper {:background-color "#f9f7ff"
+               :padding "0 50px 50px"
+               :border-radius "10px"}]])
 
 (defn render []
   [:html
@@ -35,11 +38,18 @@
    [:body
     [:div#main
      [:div {:style "margin: 0 10px"}
-      [:h1.title "lilac.town"
-       [:small "Games"]]]]
+      [:a {:href "/"}
+       [:h1.title "lilac.town"
+        [:small "Games"]]]
+      [:div [:h2 "Sweeper"]
+       [:div#sweeper
+        (client/module {:module :sweeper
+                        :init 'lilactown.client.sweeper/start!
+                        :ref :parent})]]]]
     [:link {:href "https://use.fontawesome.com/releases/v5.0.6/css/all.css"
             :rel "stylesheet"}]
     [:link {:href "https://fonts.googleapis.com/css?family=Roboto+Condensed|Roboto+Slab"
             :rel "stylesheet"}]
     [:style
-     (garden/css styles)]]])
+     (garden/css styles)]
+    (client/main)]])
